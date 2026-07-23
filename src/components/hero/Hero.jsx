6a50a21d539e864a3f692b59634424a77bd6a5b4
Ryan Nunes <ryansantana276@gmail.com>
2026-07-23 10:08:06 -0300

    hero btn add

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,14 @@
 import './hero.css'
 import bolinhoImg from "../../assets/images/paozinhobolinho.webp"
+import downArrow from "../../assets/icons/down-arrow.svg";
 
 function Hero(){
     return (
         <div className="box">
+            <section className="left">
             <span>Pães artesanais feitos diariamente para transformar cada café em um momento especial.</span>
+            <button className="btn-cardapio">Ver cardápio <img src={downArrow}/></button>
+            </section>
             <img src={bolinhoImg}/>
         </div>
     )
