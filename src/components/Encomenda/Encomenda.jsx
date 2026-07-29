c13d22a157b31e831f7057d0f715c0382ddf9640
Ryan Nunes <ryansantana276@gmail.com>
2026-07-29 12:47:42 -0300

    criação lista de produtos

--- src/components/Encomenda/Encomenda.jsx
+++ src/components/Encomenda/Encomenda.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
 import "./Encomenda.css"
 
+const produtos = [
+    {id:1,nome:"Pão Francês",preço:0.80},
+    {id:2,nome:"Croissant Artesanal",preço:6.50},
+    {id:3,nome:"Pão Italiano", preço:8.00},
+    {id:4,nome:"Sonho",preço:5.00},
+    {id:5,nome:"Bolo de Cenoura",preço:7.50},
+    {id:6,nome:"Café Especial",preço:5.00}
+]
+
 function Encomenda(){
+    const [form, setForm] = useState({ produtoId:""})
+    function handleChange(e){
+        setForm({...form, [e.target.name]: e.target.value})
+    }
     return(
         <div className="box encomendabox">
             <form className="formEncomenda">
@@ -9,7 +23,7 @@ function Encomenda(){
                 <label htmlFor="telefone">Telefone</label>
                 <input type="number" id="telefone" name="telefone" placeholder="Nome Completo"/>
                 <label htmlFor="produto">Produto</label>
-                <select id="produto" name="produto" value={form.produtoId} onChange={handleChange}>
+                <select id="produto" name="produtoId" value={form.produtoId} onChange={handleChange}>
                     {produtos.map(p => (
                         <option key={p.id} value={p.id}>{p.nome}</option>
                     ))}
